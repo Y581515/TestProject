319b326a68abac63fed1036e9df12e60bc972ea0
Elkjøp Låne-PC <Elkjp Lne-PC@DESKTOP-0QLCH2F>
2020-08-27 16:00:36 +0200

    doubleup

---
+++ DoubleUp/src/hvl/no/dat100/Dialog.java
@@ -0,0 +1,16 @@
+package hvl.no.dat100;
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.*;
+
+
+public class Dialog {
+
+	public static void main(String[] args) {
+		String tallTxt = showInputDialog("Gi et tall");
+		int tall = parseInt(tallTxt);
+		int dobbel = tall*2;
+		String UtTxt = "svar: "+ dobbel;
+		showMessageDialog(null,UtTxt);
+	}
+
+}
